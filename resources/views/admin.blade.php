879b5234f520006e39bf862b17a87fde6518b97e
Alain Tanguy <alainTanguy@zeop.re>
2021-01-26 17:19:18 +0400

    --- Auto-Git Commit ---

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,14 +1,13 @@
-@extends('home')
+@extends('template')
 
-@section('liste_du personnel')
-
+@section('contenu')
 
 <div class="p-t-15">
 <div class="col-md-10">
     <div class="card">
         <div class="card-body">
-            <h4>“L'abondance est le fruit d'une bonne administration.”</h4>
-            <h4 class="card-title">Custom Tab 1</h4>
+            <!--<h4>“L'abondance est le fruit d'une bonne administration.”</h4>
+            <h4 class="card-title">Custom Tab 1</h4>-->
             <!-- Nav tabs -->
             <div class="custom-tab-1">
                 <ul class="nav nav-tabs mb-3">
@@ -18,9 +17,6 @@
                     </li>
                     <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#historique">Historique</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#register">Enregistrement</a>
-                    </li>
-
                 </ul>
                 <div class="tab-content">
                     <div class="tab-pane fade show active" id="utilisateur" role="tabpanel">
@@ -72,11 +68,69 @@
                                         </tbody>
                                     </table>
                                 </div>
+
+                                <div class="col-xl-5">
+
+                                    <div class="card-body">
+                                        <h4>Enregistrer un utilisateur</h4>
+                                        <form class="user">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control form-control-user" id="first_name" aria-describedby="firstNameHelp" placeholder="Prénom">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" class="form-control form-control-user" id="last_name" aria-describedby="lastNameHelp" placeholder="Nom">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="email">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" class="form-control form-control-user" id="phone" aria-describedby="phoneHelp" placeholder="téléphone">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Mot de passe">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Confirmer le mot de passe">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <select id="magasin" class="form-control" value="{{ old('magasin') }}" required>
+                                                    <option selected>Magasin</option>
+                                                    <option>Santa Cruz</option>
+                                                    <option>Baldwin</option>
+                                                    <option>Rowlett</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <select id="manager" class="form-control" value="{{ old('profil') }}" required>
+                                                    <option selected>Responsable direct</option>
+                                                    <option>Vendeur</option>
+                                                    <option>Administratif</option>
+                                                    <option>Préparateur</option>
+                                                    <option>Réparateur</option>
+                                                    <option>Direction</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <p class="form-control form-control-user">
+                                                    Personnel en activité &nbsp;? &nbsp;&nbsp;&nbsp;
+                                                    <label class="radio-inline"><input type="radio" name="active" checked> &nbsp; Oui</label>&nbsp;&nbsp;&nbsp;
+                                                    <label class="radio-inline"><input type="radio" name="active"> &nbsp; Non</label>
+                                                </p>
+                                            </div>
+
+                                            <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                                Enregistrer
+                                            </a>
+
+                                        </form>
+                                        <hr>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="tab-pane fade" id="register">
-                        @include('register')
                     </div>
 
                     <div class="tab-pane fade" id="profil">
