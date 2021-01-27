4eaa2ee36757fc6a8d3b7b12b610c3da641ed7e7
Alain Tanguy <alainTanguy@zeop.re>
2021-01-27 05:56:22 +0400

    --- Auto-Git Commit ---

--- resources/views/formCategory.blade.php
+++ resources/views/formCategory.blade.php
@@ -2,29 +2,18 @@
 
 @section('contenu')
 
-<h4>MODIFIER le nom de la catégorie</h4>
 
 <form class="col-md-6" name="categorY" action="{{ !empty($category) ? url('/category/'.$category->getId().'/edit') : url('/category/create')}}" method="post">
-    @csrf
-    <div class="form-group row, form-categorY">
-        <div class="error">{{ $errors->has("category") ? $errors->first("category") : "" }}</div>
 
-        <label for="categories" class="col-md-3 col-form-label">Nom :</label>
-        <div class="col-md-9">
-            <input name="categorY" type="text" class="form-control form-control-user" id="categorY" placeholder= "Nouvelle categorie" value="{{ !empty($category) ? $category->getName()  : "" }}">
-        </div>
+    @csrf
+    <div class="form">
+        
+        <input name="category" type="text" class="form-control form-control-user" id="category" placeholder="New category" value="{{ !empty($category) ? $category->getName()  : "" }}">
+
     </div>
-    <div class="form-group row">
-        <div class="col">
-           <input class="btn btn-primary btn-user btn-block" type="submit" value="submit">
-        </div>
-    </div>
+    <br>
+    <button type="submit" class="btn btn-primary btn-user btn-block">Enregistrer</button>
 </form>
 
+
 @stop
-
-
-
-
-
-
