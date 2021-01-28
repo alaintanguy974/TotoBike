6b9c6cb69a1cb010d297b87c3ea9aa84be390acb
Alain Tanguy <alainTanguy@zeop.re>
2021-01-28 17:14:54 +0400

    --- Auto-Git Commit ---

--- resources/views/formProduct.php
+++ resources/views/formProduct.php
@@ -3,19 +3,17 @@
 
 @section('create')
 
-<h4>MODIFIER le nom du produit</h4>
-
-<form class="col-md-6" name="product" action="{{ !empty($product) ? url('/product/'.$category->getId().'/edit') : url('/product/create')}}" method="POST">
-    @csrf
-    <div class="form-group row, form-product">
-        <label for="product" class="col-md-3 col-form-label">Nom :</label>
-        <div class="col-md-9">
-            <input name="product" type="text" class="form-control col-md-3 col form-control-user" id="product" placeholder="Nouveau produit" value="{{ !empty($product) ? $product->getName()  : "" }}">
-        </div>
-    </div>
-    <div class="form-group row">
-        <div class="col">
-            <input class="btn btn-primary btn-user btn-block" type="submit" value="submit">
-        </div>
-    </div>
+<h4>CREATION d'une nouveau produit</h4>
+<form name="brand" action="">
+    <div class="form-produit">
+        <input type="text" class="form-control form-control-user" id="brand" placeholder="Nom du produit">
+    </div><br>
+    <select name="" id="" class="form-control form-control-user">
+        <option value="allBrands">Choisir une marques</option>
+    </select><br>
+    <select name="" id="" class="form-control form-control-user">
+        <option value="allCategory">Choisir une catégorie</option>
+    </select>
+    <br>
+    <button type="submite" class="btn btn-primary btn-user btn-block">Enregistrer</button>
 </form>
