5a9673db052c7a950c47ef4e92342eb8c10120ea
Alain Tanguy <alainTanguy@zeop.re>
2021-01-28 06:28:56 +0400

    --- Auto-Git Commit ---

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,42 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\BrandException;
 use App\Models\Manager\BrandManagerInterface;
 use App\Models\Manager\CategoryManagerInterface;
 use App\Models\Manager\ProductManagerInterface;
 use App\Models\Manager\StockManagerInterface;
 use App\Models\Manager\StoreManagerInterface;
 use App\Models\Manager\StaffManagerInterface;
-
-
-
 class HomeController extends Controller{
 
     public function index(
         ProductManagerInterface $productManager, 
-    BrandManagerInterface $brandManager, 
-    StockManagerInterface $stockManager, 
-    CategoryManagerInterface $categoryManager,
-    StoreManagerInterface $storeManager,
-    StaffManagerInterface $staffManager){
+        BrandManagerInterface $brandManager, 
+        StockManagerInterface $stockManager, 
+        CategoryManagerInterface $categoryManager,
+        StoreManagerInterface $storeManager,
+        StaffManagerInterface $staffManager){
 
        
-        $allBrands = $brandManager->getAllBrands();
-        $allProducts = $productManager->getAllProducts();
-        $allStocks = $stockManager->getAllStocks();
-        $allCategories = $categoryManager->getAllCategories();
-        $allStores =  $storeManager->getAllStores();
-        $allStaffs =  $staffManager->getAllStaffs();
+            $allBrands = $brandManager->getAllBrands();
+            $allProducts = $productManager->getAllProducts();
+            $allStocks = $stockManager->getAllStocks();
+            $allCategories = $categoryManager->getAllCategories();
+            $allStores =  $storeManager->getAllStores();
+            $allStaffs =  $staffManager->getAllStaffs();
 
-        return view('home')->with([
-            "brand" => $allBrands,
-            "product" => $allProducts,            
-            "stock" => $allStocks,
-            "category" => $allCategories,
-            "store" => $allStores,
-            "staff" => $allStaffs,
-        ]);
+            return view('home')->with([
+                "brand" => $allBrands,
+                "product" => $allProducts,            
+                "stock" => $allStocks,
+                "category" => $allCategories,
+                "store" => $allStores,
+                "staff" => $allStaffs,
+            ]);
     }
 
 }
